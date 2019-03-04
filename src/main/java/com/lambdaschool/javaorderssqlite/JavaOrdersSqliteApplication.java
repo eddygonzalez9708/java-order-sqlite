88368d8525fe79bb1e8415fedeb715effab91e31
Eddy <edward.gonzalez9708@gmail.com>
2019-03-04 14:24:39 -0500

    first commit

---
+++ src/main/java/com/lambdaschool/javaorderssqlite/JavaOrdersSqliteApplication.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.javaorderssqlite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaOrdersSqliteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaOrdersSqliteApplication.class, args);
+    }
+
+}
