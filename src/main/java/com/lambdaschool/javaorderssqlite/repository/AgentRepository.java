3cbf30f4ada29080bb87f989ca3af7dab6a85ce9
Eddy <edward.gonzalez9708@gmail.com>
2019-03-04 18:23:12 -0500

    Created Jpa repository for each base class in the models folder.

---
+++ src/main/java/com/lambdaschool/javaorderssqlite/repository/AgentRepository.java
@@ -0,0 +1,12 @@
+package com.lambdaschool.javaorderssqlite.repository;
+
+import com.lambdaschool.javaorderssqlite.models.Agents;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface AgentRepository extends JpaRepository<Agents, Long> {
+    @Query(value = "SELECT a.agentname, o.ordnum, o.orddescription FROM Agents a, Orders o WHERE a.agentcode = o.agentcode ORDER BY agentname", nativeQuery = true)
+    List<Object> findAgentOrders();
+}
---
+++ src/main/java/com/lambdaschool/javaorderssqlite/repository/OrderRepository.java
@@ -0,0 +1,16 @@
+package com.lambdaschool.javaorderssqlite.repository;
+
+import com.lambdaschool.javaorderssqlite.models.Orders;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Orders, Long> {
+    @Query(value = "SELECT o.ordnum, o.advanceamount, o.ordamount, o.orddescription FROM Orders o WHERE o.custcode IN (SELECT c.custcode FROM Customers c WHERE c.custname = :custname)", nativeQuery = true)
+    List<Object> findCustomerOrdersByName(@Param("custname") String custname);
+
+    @Query(value = "SELECT o.ordnum, o.advanceamount, o.ordamount, o.orddescription FROM Orders o WHERE o.custcode = :custcode", nativeQuery = true)
+    List<Object> findCustomerOrdersByCustCode(@Param("custcode") long custname);
+}
